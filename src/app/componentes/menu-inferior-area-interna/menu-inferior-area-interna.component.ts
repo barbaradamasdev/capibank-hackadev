8f4dcca461c048c0f80a0fba10b37297d85f09d5
Regis Medeiros <medeirosregis90@gmail.com>
2024-02-10 18:52:39 -0300

    criado rodapé do componente menu-inferior-area-interna com alteracoes em arquivos png do banco de imagens

--- src/app/componentes/menu-inferior-area-interna/menu-inferior-area-interna.component.ts
+++ src/app/componentes/menu-inferior-area-interna/menu-inferior-area-interna.component.ts
@@ -9,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class MenuInferiorAreaInternaComponent {
 
+  // Definindo as imagens iniciais e cores das legendas
+  icons = [
+    { src: 'assets/icones/icone-inicio-cinza.png', alt: 'Início', color: '#666' },
+    { src: 'assets/icones/icone-transacoes-cinza.png', alt: 'Transações', color: '#666' },
+    { src: 'assets/icones/icone-cartoes-cinza.png', alt: 'Meus cartões', color: '#666' },
+    { src: 'assets/icones/icone-config-cinza.png', alt: 'Configurações', color: '#666' }
+  ];
+
+  constructor() { }
+
+  // Método para alterar a imagem e a cor da legenda quando o mouse entra
+  onMouseEnter(icon: any) {
+    icon.src = icon.src.replace('-cinza.png', '-azul.png');
+    icon.color = '#23217E';
+  }
+
+  // Método para reverter a imagem e a cor da legenda quando o mouse sai
+  onMouseLeave(icon: any) {
+    icon.src = icon.src.replace('-azul.png', '-cinza.png');
+    icon.color = '#666';
+  }
+
 }
