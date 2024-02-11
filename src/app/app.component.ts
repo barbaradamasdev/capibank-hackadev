8ec38b31268be699f9f1adbd68a05d1dfef8bf3c
Marcelo Jhones <marceloj@acad.ifma.edu.br>
2024-02-11 00:48:29 -0300

    feat: adicao do html e css na pagina login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { InicioLoginComponent } from "./pages/inicio-login/inicio-login.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, InicioLoginComponent]
 })
 export class AppComponent {
   title = 'capibank';
