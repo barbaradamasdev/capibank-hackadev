4990ff67d320542f1a7d86b2a9b92863b98bc72d
Jackson Moraes <jacksontadeu@hotmail.com>
2024-02-11 16:51:55 -0300

    :lipstick:feat: Criado o componente cabecalho e acertado o route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AreaDoClienteComponent } from './pages/area-do-cliente/area-do-cliente.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'cliente',component:AreaDoClienteComponent},
+    {path:'',redirectTo:'cliente',pathMatch:'full'}
+];
