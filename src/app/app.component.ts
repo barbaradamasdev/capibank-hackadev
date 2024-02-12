f87d7c24071cf1113434ea31cc073f55577e929a
Ana Júlia Medeiros <julinhamedeiros2013@gmail.com>
2024-02-11 22:28:01 -0300

    atualizações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { InicioLoginComponent } from "./pages/inicio-login/inicio-login.component";
-
+import { InicioCriarContaComponent } from './pages/inicio-criar-conta/inicio-criar-conta.component';
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, InicioLoginComponent]
+    imports: [CommonModule, RouterOutlet, InicioCriarContaComponent]
 })
 export class AppComponent {
   title = 'capibank';
