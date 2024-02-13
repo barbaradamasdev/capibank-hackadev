2a877842b238e2aad6b8756fdff96a11f423cbcc
Silvana23 <silvanasantana211756@gmail.com>
2024-02-13 19:23:51 -0300

    criacao de tela recuperar senha

--- src/app/components/formulario-recuperar-criar-nova-senha/formulario-recuperar-criar-nova-senha.component.ts
+++ src/app/components/formulario-recuperar-criar-nova-senha/formulario-recuperar-criar-nova-senha.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-formulario-recuperar-criar-nova-senha',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './formulario-recuperar-criar-nova-senha.component.html',
   styleUrl: './formulario-recuperar-criar-nova-senha.component.css'
 })
 export class FormularioRecuperarCriarNovaSenhaComponent {
+  newPassword: string = '';
+  confirmPassword: string = '';
+  passwordRequirements = {
+    length: false,
+    number: false,
+    lowercase: false,
+    uppercase: false
+  };
+
+  checkPasswordRequirements() {
+    const numberPattern = /\d/; // Padrão para detectar números
+    const lowercasePattern = /[a-z]/; // Padrão para detectar letras minúsculas
+    const uppercasePattern = /[A-Z]/; // Padrão para detectar letras maiúsculas
+
+    this.passwordRequirements.length = this.newPassword.length >= 8;
+    this.passwordRequirements.number = numberPattern.test(this.newPassword);
+    this.passwordRequirements.lowercase = lowercasePattern.test(this.newPassword);
+    this.passwordRequirements.uppercase = uppercasePattern.test(this.newPassword);
+  }
 
 }
