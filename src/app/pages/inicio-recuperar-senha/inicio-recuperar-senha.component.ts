c6c9e9725913e7e72d887c624e144a4953063e45
Barbara Damasceno <barbaradamas.dev@gmail.com>
2024-02-16 09:39:42 -0300

    Altera importacao pagina recuperar senha

--- src/app/pages/inicio-recuperar-senha/inicio-recuperar-senha.component.ts
+++ src/app/pages/inicio-recuperar-senha/inicio-recuperar-senha.component.ts
@@ -8,13 +8,15 @@ import { EsqueciSenhaLoginComponent } from "../../componentes/esqueci-senha-logi
 import { FormularioCriarNomeComponent } from "../../componentes/formulario-criar-nome/formulario-criar-nome.component";
 import { FormularioRecuperarCriarNovaSenhaComponent } from "../../components/formulario-recuperar-criar-nova-senha/formulario-recuperar-criar-nova-senha.component";
 import { FormularioRecuperarPgUmComponent } from "../../componentes/formulario-recuperar-pg-um/formulario-recuperar-pg-um.component";
+import { FormularioRecuperarPgTresComponent } from "../../componentes/formulario-recuperar-pg-tres/formulario-recuperar-pg-tres.component";
+import { FormularioRecuperarPgDoisComponent } from "../../componentes/formulario-recuperar-pg-dois/formulario-recuperar-pg-dois.component";
 
 @Component({
     selector: 'app-inicio-recuperar-senha',
     standalone: true,
     templateUrl: './inicio-recuperar-senha.component.html',
     styleUrl: './inicio-recuperar-senha.component.css',
-    imports: [CabecalhoLoginComponent, RodapeLoginComponent, FormularioRecuperarCodigoComponent, FormularioCriarSucessoComponent, CriarContaLinkComponent, EsqueciSenhaLoginComponent, FormularioCriarNomeComponent, FormularioRecuperarCriarNovaSenhaComponent, FormularioRecuperarPgUmComponent]
+    imports: [CabecalhoLoginComponent, FormularioRecuperarPgUmComponent, RodapeLoginComponent, FormularioRecuperarPgTresComponent, FormularioRecuperarPgDoisComponent]
 })
 export class InicioRecuperarSenhaComponent {
 
