9d80187b2d4925319d014a5c49ab988f8aea47c5
Jackson Moraes <jacksontadeu@hotmail.com>
2024-02-15 21:24:48 -0300

    :lipstick: Criacao das rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AreaDoClienteComponent } from './pages/area-do-cliente/area-do-cliente.component';
 import { InicioLoginComponent } from './pages/inicio-login/inicio-login.component';
+import { InicioCriarContaComponent } from './pages/inicio-criar-conta/inicio-criar-conta.component';
 
 export const routes: Routes = [
     {path:'cliente',component:AreaDoClienteComponent},
     {path:'inicio',component:InicioLoginComponent},
+    {path:'criarconta',component:InicioCriarContaComponent},
     {path:'',redirectTo:'inicio',pathMatch:'full'}
 ];
