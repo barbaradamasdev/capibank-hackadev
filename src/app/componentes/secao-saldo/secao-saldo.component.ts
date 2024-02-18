f84cb3db389ed5f222a7a86d5149977e299326f2
Barbara Damasceno <barbaradamas.dev@gmail.com>
2024-02-18 11:51:45 -0300

    Adiciona botao de ocultar saldo

--- src/app/componentes/secao-saldo/secao-saldo.component.ts
+++ src/app/componentes/secao-saldo/secao-saldo.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-secao-saldo',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './secao-saldo.component.html',
   styleUrl: './secao-saldo.component.css'
 })
 export class SecaoSaldoComponent {
   nome = localStorage.getItem('nome')
+
+  saldoVisivel: boolean = true;
+
+  toggleVisibilidadeSaldo() {
+    this.saldoVisivel = !this.saldoVisivel;
+  }
 }
