74bc00764e5c3e5640a4341a2d0b18f1791aa8bc
Barbara Damasceno <barbaradamas.dev@gmail.com>
2024-02-18 11:27:47 -0300

    Insere rota no menu lateral

--- src/app/componentes/menu-lateral/menu-lateral.component.ts
+++ src/app/componentes/menu-lateral/menu-lateral.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu-lateral',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './menu-lateral.component.html',
   styleUrl: './menu-lateral.component.css'
 })
 export class MenuLateralComponent {
-
 }
