7f4c13bf313647a6f7d1acba22ca146eefd8d0cd
Barbara Damasceno <barbaradamas.dev@gmail.com>
2024-02-18 11:27:05 -0300

    Insere estilo overflow na tela de historico

--- src/app/componentes/historico-transacoes/historico-transacoes.component.ts
+++ src/app/componentes/historico-transacoes/historico-transacoes.component.ts
@@ -2,15 +2,32 @@ import { Component } from '@angular/core';
 import { Transacao } from '../../Models/Transacao';
 import { TransacaoTipo } from '../../Models/TransacaoTipo';
 import { CommonModule } from '@angular/common';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-historico-transacoes',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './historico-transacoes.component.html',
   styleUrl: './historico-transacoes.component.css'
 })
 export class HistoricoTransacoesComponent {
+
+  aplicarEstiloOverflow: boolean = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const url = event.url;
+
+        if (url.endsWith('/historico')) {
+          this.aplicarEstiloOverflow = true;
+        } else {
+          this.aplicarEstiloOverflow = false;
+        }
+      }
+    });
+  }
 
   transacoes: Transacao[] =[
     {transacaoTipo: TransacaoTipo.entrada, tipo:'Pix Recebido', valor: 1000.00, motivo:'José Pedro', data: '15/01/2024'},
@@ -26,6 +43,8 @@ export class HistoricoTransacoesComponent {
     let operacaoTransacao = (t.transacaoTipo === TransacaoTipo.saida) ? '-' : '+';
     return `${operacaoTransacao}${t.valor}`;
   }
+
+
 
 
 }
