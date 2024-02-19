7111df172ce0e3b69d547c20a97ce436783aabb1
Barbara Damasceno <barbaradamas.dev@gmail.com>
2024-02-19 10:21:18 -0300

    Insere logout no menu lateral

--- src/app/componentes/menu-lateral/menu-lateral.component.ts
+++ src/app/componentes/menu-lateral/menu-lateral.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -9,4 +9,13 @@ import { RouterLink } from '@angular/router';
   styleUrl: './menu-lateral.component.css'
 })
 export class MenuLateralComponent {
+  @Output() linkClicado: EventEmitter<void> = new EventEmitter<void>();
+
+  onLinkClick() {
+    this.linkClicado.emit();
+  }
+
+  logout():void{
+    localStorage.clear();
+  }
 }
