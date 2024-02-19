aca6922e8e275bf18a37f1a85550e12b33923873
Barbara Damasceno <barbaradamas.dev@gmail.com>
2024-02-19 10:21:54 -0300

    Remove importacoes nao usadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,20 +3,15 @@ import { AreaDoClienteComponent } from './pages/area-do-cliente/area-do-cliente.
 import { InicioRecuperarSenhaComponent } from './pages/inicio-recuperar-senha/inicio-recuperar-senha.component';
 import { InicioCriarContaComponent } from './pages/inicio-criar-conta/inicio-criar-conta.component';
 import { InicioLoginComponent } from './pages/inicio-login/inicio-login.component';
-import { HistoricoTransacoesComponent } from './componentes/historico-transacoes/historico-transacoes.component';
 import { HistoricoDeTransacaoComponent } from './pages/historico-de-transacao/historico-de-transacao.component';
 import { FormularioCriarContaPgUmComponent } from './pages/inicio-criar-conta/forms/formulario-criar-conta-pg-um/formulario-criar-conta-pg-um.component';
 import { FormularioCriarContaPgDoisComponent } from './pages/inicio-criar-conta/forms/formulario-criar-conta-pg-dois/formulario-criar-conta-pg-dois.component';
 import { FormularioCriarContaPgTresComponent } from './pages/inicio-criar-conta/forms/formulario-criar-conta-pg-tres/formulario-criar-conta-pg-tres.component';
+import { FormularioCriarContaPgQuatroComponent } from './pages/inicio-criar-conta/forms/formulario-criar-conta-pg-quatro/formulario-criar-conta-pg-quatro.component';
 import { autenticarGuard } from './seguranca/autenticar.guard';
-
-import { FormularioRecuperarPgUmComponent } from './componentes/formulario-recuperar-pg-um/formulario-recuperar-pg-um.component';
-import { FormularioRecuperarPgTresComponent } from './componentes/formulario-recuperar-pg-tres/formulario-recuperar-pg-tres.component';
-import { FormularioRecuperarPgDoisComponent } from './componentes/formulario-recuperar-pg-dois/formulario-recuperar-pg-dois.component';
-import { FormularioCriarSucessoComponent } from './componentes/formulario-criar-sucesso/formulario-criar-sucesso.component';
-import { FormularioCriarContaPgQuatroComponent } from './pages/inicio-criar-conta/forms/formulario-criar-conta-pg-quatro/formulario-criar-conta-pg-quatro.component';
-import { BannerComponent } from './componentes/banner/banner.component';
-import { FormularioCriarContaPgQuatroComponent } from './pages/inicio-criar-conta/forms/formulario-criar-conta-pg-quatro/formulario-criar-conta-pg-quatro.component';
+import { FormularioRecuperarPgUmComponent } from './pages/inicio-recuperar-senha/forms/formulario-recuperar-pg-um/formulario-recuperar-pg-um.component';
+import { FormularioRecuperarPgDoisComponent } from './pages/inicio-recuperar-senha/forms/formulario-recuperar-pg-dois/formulario-recuperar-pg-dois.component';
+import { FormularioRecuperarPgTresComponent } from './pages/inicio-recuperar-senha/forms/formulario-recuperar-pg-tres/formulario-recuperar-pg-tres.component';
 
 
 export const routes: Routes = [
--- src/app/pages/inicio-recuperar-senha/inicio-recuperar-senha.component.ts
+++ src/app/pages/inicio-recuperar-senha/inicio-recuperar-senha.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { CabecalhoLoginComponent } from "../../componentes/cabecalho-login/cabecalho-login.component";
 import { RodapeLoginComponent } from "../../componentes/rodape-login/rodape-login.component";
-import { FormularioRecuperarPgUmComponent } from "./forms/formulario-recuperar-pg-um/formulario-recuperar-pg-um.component";
-import { FormularioRecuperarPgDoisComponent } from './forms/formulario-recuperar-pg-dois/formulario-recuperar-pg-dois.component';
-import { FormularioRecuperarPgTresComponent } from "./forms/formulario-recuperar-pg-tres/formulario-recuperar-pg-tres.component";
 import { BannerComponent } from "../../componentes/banner/banner.component";
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-inicio-recuperar-senha',
