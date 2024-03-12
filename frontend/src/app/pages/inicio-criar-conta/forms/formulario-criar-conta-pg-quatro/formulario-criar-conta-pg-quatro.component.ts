74b081a0967a3cf49309ba895ac00f957955c394
Barbara Damasceno <barbaradamas.dev@gmail.com>
2024-03-12 13:26:10 -0300

    Atualiza paginas para receber backend

---
+++ frontend/src/app/pages/inicio-criar-conta/forms/formulario-criar-conta-pg-quatro/formulario-criar-conta-pg-quatro.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-formulario-criar-conta-pg-quatro',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './formulario-criar-conta-pg-quatro.component.html',
+  styleUrls: ['../../../../../form.css', './formulario-criar-conta-pg-quatro.component.css']
+
+})
+export class FormularioCriarContaPgQuatroComponent {
+
+}
