781de8461b3199e8f8a0116c16a68cd04a335e02
Ana Júlia Medeiros <julinhamedeiros2013@gmail.com>
2024-03-17 20:47:01 -0300

    Finalizando

--- backend/Troopers.Capibank/obj/Debug/net8.0/Troopers.Capibank.AssemblyInfo.cs
+++ backend/Troopers.Capibank/obj/Debug/net8.0/Troopers.Capibank.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Troopers.Capibank")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f44781c1ab79076bb568e1daf61c8b1be01b67a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfa528536473295a54bec980294b5ffd9e4bc5ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Troopers.Capibank")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Troopers.Capibank")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
