2a90b8262165f575c16e5fcbae8054eb7624f351
Barbara Damasceno <barbaradamas.dev@gmail.com>
2024-03-18 10:37:20 -0300

    Paginas chamado, config

--- backend/Troopers.Capibank/obj/Debug/net8.0/Troopers.Capibank.AssemblyInfo.cs
+++ backend/Troopers.Capibank/obj/Debug/net8.0/Troopers.Capibank.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Troopers.Capibank")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f44781c1ab79076bb568e1daf61c8b1be01b67a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30adcda988cf387382d686cb374a5770a7f12814")]
 [assembly: System.Reflection.AssemblyProductAttribute("Troopers.Capibank")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Troopers.Capibank")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
