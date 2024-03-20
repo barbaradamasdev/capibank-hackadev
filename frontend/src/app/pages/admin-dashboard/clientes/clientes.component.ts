3f5530b2ea613ae383d2a5114d65aaa93dd21c5c
Barbara Damasceno <barbaradamas.dev@gmail.com>
2024-03-20 08:55:58 -0300

    Estrutura pagina clientes admin

--- frontend/src/app/pages/admin-dashboard/clientes/clientes.component.ts
+++ frontend/src/app/pages/admin-dashboard/clientes/clientes.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 import { FormChamadoComponent } from "../../../pages/area-do-cliente/chamado/form-chamado/form-chamado.component";
-import { Subscription, filter } from 'rxjs';
-import { MenuService } from '../../../servicos/menu.service';
-import { NavigationEnd, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MenuLateralAdminComponent } from "../menu-lateral-admin/menu-lateral-admin.component";
 import { DashboardComponent } from "../dashboard/dashboard.component";
@@ -18,30 +15,6 @@ import { FormsModule } from '@angular/forms';
     imports: [FormsModule, CommonModule, FormChamadoComponent, MenuLateralAdminComponent, DashboardComponent]
 })
 export class ClientesComponent {
-  isMenuOpen: boolean = false;
-  private routerSubscription: Subscription;
-
-  constructor(private menuService: MenuService, private router: Router) {
-    this.menuService.isMenuOpen$.subscribe((isOpen: boolean) => {
-      this.isMenuOpen = isOpen;
-    });
-
-    this.routerSubscription = this.router.events.pipe(
-      filter((event) => event instanceof NavigationEnd)
-    ).subscribe(() => {
-      this.isMenuOpen = false;
-    });
-  }
-
-  onLinkClicked() {
-    this.isMenuOpen = false;
-  }
-
-  ngOnDestroy(): void {
-    if (this.routerSubscription) {
-      this.routerSubscription.unsubscribe();
-    }
-  }
 
   filtro: string = '';
   filtroTipo: string = 'nome';
