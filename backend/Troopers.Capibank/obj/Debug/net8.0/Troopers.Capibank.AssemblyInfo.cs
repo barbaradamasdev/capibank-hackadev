f02d10f66c5b2084ec3e80956825dcf53aeaafa0
Barbara Damasceno <barbaradamas.dev@gmail.com>
2024-03-25 21:16:22 -0300

    Atualiza pix api service

--- backend/Troopers.Capibank/obj/Debug/net8.0/Troopers.Capibank.AssemblyInfo.cs
+++ backend/Troopers.Capibank/obj/Debug/net8.0/Troopers.Capibank.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Troopers.Capibank")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a45d8d7280f7c50f7709d21cf38e7d3fc50754ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b042fea54bc3392ce7764e9cd4a89130e6624082")]
 [assembly: System.Reflection.AssemblyProductAttribute("Troopers.Capibank")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Troopers.Capibank")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
